af887f67996f9cd9c00730b8c757cebaec91a98c
Michael Jasinski <michael.jasinski@detroitlabs.com>
2016-06-01 18:08:50 -0400

    First commit pics are inplace

---
+++ PhotoLabs/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  PhotoLabs
+//
+//  Created by DetroitLabs on 5/31/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//-(NSInteger)collectionView:(UICollectionView*)collectionView numberOfItemsInSection:(NSInteger)section {
+//    return 10;
+//}
+//
+//
+//- (UICollectionViewCell*)collectionView:(UICollectionView*)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath{
+//    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Photo" forIndexPath:indexPath];
+//    cell.backgroundColor = [UIColor lightGrayColor];
+//    return cell;
+//}
+
+
+@end
